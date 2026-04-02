1ff56bdc4cb5a88c780794bd9b112295c8a08806
Radhika8296 <radhikaa882002@gmail.com>
2026-04-02 23:47:20 +0530

    Add files via upload

---
+++ uanalytics.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Superman_Web
+{
+    public partial class uanalytics : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+        }
+
+        protected void RowDelete(Object sender, GridViewDeletedEventArgs e)
+        {
+
+        }
+
+    }
+}
